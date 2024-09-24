f3154d2119be61b1406c2c4ea1a5e6871cac5e94
Ananyas24 <143926350+Ananyas24@users.noreply.github.com>
2024-09-24 10:34:57 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Correct the base URL here
+  baseURL: 'https://event-management-backend-zl2d.onrender.com';  // Correct the base URL here
   headers: {
     'Content-Type': 'application/json',
   },
