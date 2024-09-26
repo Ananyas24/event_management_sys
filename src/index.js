0fbf62920fb756b867e1e3fa40ad51e70aeeb803
Ananya <srivastavaananya194@gmail.com>
2024-09-26 18:41:59 +0530

    remove store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import store from './store/store';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';  // Import Toastify CSS
 
